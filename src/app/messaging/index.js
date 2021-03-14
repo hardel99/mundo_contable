258685207401f997c20ca4786a9ad4ea4ee2266e
hardel99 <harold-delgado1999@hotmail.com>
2021-03-14 10:13:21 -0600

    added firebase auth

--- src/app/messaging/index.js
+++ src/app/messaging/index.js
@@ -4,10 +4,23 @@ var authToken = "bc29df4c46add674f3c351a573c49b15"; // Your Auth Token from www.
 var twilio = require("twilio");
 var client = new twilio(accountSid, authToken);
 
-client.messages
-    .create({
-        body: "hello there ewe",
-        to: "+50378133409", // Text this number
-        from: "+15053023912", // From a valid Twilio number
-    })
-    .then((message) => console.log(message.sid));
+/**Params:
+ * text - the text message
+ * number - the nummber what would recieve the message, mus have the format +???actualNumber,
+ * where ??? is the country extension & and actual number is, well teh actual number
+ */
+/**
+ * TODO:
+ * ?Restrictions for sending a message(freemium plan)
+ * ?Contact list for user
+ * ?Register/Login
+ */
+function sendMessage(text, number) {
+    client.messages
+        .create({
+            body: text,
+            to: number,
+            from: "+15053023912",
+        })
+        .then((message) => console.log(message.sid));
+}
