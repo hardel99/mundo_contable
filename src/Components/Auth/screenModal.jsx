449868b0a7b671dfb5df21da6a156a45f4407eb1
M-Mag <villegasmkenya147@gmail.com>
2021-04-21 14:17:55 -0600

    login/signup works

--- src/Components/Auth/screenModal.jsx
+++ src/Components/Auth/screenModal.jsx
@@ -20,7 +20,7 @@ const Background = styled.div`
 const ModalWrapper = styled.div`
     width: 834px;
     height: 600px;
-    box-shadow: 0 5px 16px white;
+    box-shadow: 0 5px 16px rgba(0, 0, 0, 0.7);
     color: #000;
     display: grid;
     grid-template-columns: 1fr 1fr;
