33e6122162199b33ec140c41e6c0f89d782296ff
hardel99 <harold-delgado1999@hotmail.com>
2021-04-22 15:42:40 -0600

    added functionallity to Comenzar button

--- src/Components/HeroSection/index.jsx
+++ src/Components/HeroSection/index.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import 
-{
+import React from "react";
+import {
     HeroContainer,
     HeroContent,
     HeroItems,
     HeroH1,
     HeroP,
-    HeroBtn
-} from './HeroElements'
+    HeroBtn,
+} from "./HeroElements";
 
 const Hero = () => {
     return (
         <HeroContainer>
             <HeroContent>
                 <HeroItems>
-                   <HeroH1>Mundo Contable</HeroH1>
-             
-                    <HeroP>Te ofrecemos una plataforma global y hacemos que sea mas fácil realizar tu contabilidad como empresario o emprendedor.</HeroP>
-                    <HeroBtn>Comenzar</HeroBtn>
+                    <HeroH1>Mundo Contable</HeroH1>
+
+                    <HeroP>
+                        Te ofrecemos una plataforma global y hacemos que sea mas
+                        fácil realizar tu contabilidad como empresario o
+                        emprendedor.
+                    </HeroP>
+                    <a href="#servicios">
+                        <HeroBtn>Comenzar</HeroBtn>
+                    </a>
                 </HeroItems>
             </HeroContent>
         </HeroContainer>
     );
-}
+};
 
 export default Hero;
