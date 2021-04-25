659c8e6068a68af7cfc5fde902cda9eec29f36ed
hardel99 <harold-delgado1999@hotmail.com>
2021-04-24 21:10:53 -0600

    Refactores Project Structure

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ import ForgotPassword from "./Components/Auth/restorePass";
 import PrivateRoute from "./app/auth/PrivateRoute";
 import { GlobalStyle } from "./Components/GlobalStyles";
 import Hero from "./Components/HeroSection";
-import Navbar from "./Components/navbar/navbar";
+import Navbar from "./Components/Navbar/navbar";
 import InfoSection from "./Components/InfoSection";
 import NotFound from "./Components/inexistentPage";
 import BrandSection from "./Components/BrandSection";
-
 
 /**Usuario de prueba :
  * ttt@ttt.com
@@ -29,7 +28,7 @@ function App() {
                         <section id="servicios">
                             <InfoSection />
                         </section>
-                        <BrandSection/>
+                        <BrandSection />
                         <section id="nosotros"></section>
                         <section id="contacto"></section>
                     </Route>
