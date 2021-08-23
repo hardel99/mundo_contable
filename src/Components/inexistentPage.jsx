e9e00a4f692b781ce8a514457e139c9c34b13e1e
Salvador11 <salvadorsalguero01@gmail.com>
2021-08-22 23:04:32 -0600

    return button of 404 page

--- src/Components/inexistentPage.jsx
+++ src/Components/inexistentPage.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 import sample from "./../images/building.svg";
+import Button from '@material-ui/core/Button';
 
-const NfSection = styled.div `
+
+const NfSection = styled.div`
    display: flex;
    justify-content: center; 
    align-items: center;
    padding:40px;
    height :100vh;
    position:relative;
-   margin:50px;
 
 
     @media screen and (max-width: 600px) {
         margin:20px;
         padding:20px;
-
   } 
 
 `;
@@ -27,8 +27,7 @@ const Container = styled.div`
     align-items:center;
     flex-direction:column;
     text-align: center;
-/*     border:solid 2px #6ad8d8 ;
-*/
+
    
     
     .tagline 
@@ -44,9 +43,10 @@ const Container = styled.div`
 
 `;
 
-const ImageWrapper = styled.div `
+const ImageWrapper = styled.div`
      width:500px;
      margin-left:60px;
+     margin-bottom: 60px;
 
      @media screen and (max-width: 600px) {
         width:100%;
@@ -60,17 +60,20 @@ const ImageWrapper = styled.div `
 export default function NotFound() {
     return (
         <NfSection>
-        <Container>
-            <h1>PAGINA EN MANTENIMIENTO</h1>          
-            <h2>
-                Esta parte de la pagina se encuentra bloqueada por el momento, por razones de seguridad y mantenimiento, para regresar, hacer click en la flecha de retroceso, en el navegador
-            </h2>
-            <h2 className="tagline">Precaución, zona de construcción:</h2>
-            <br />
-            <ImageWrapper>
-            <img src={sample} alt={""} />
-            </ImageWrapper>
-        </Container>
+            <Container>
+                <h1>PAGINA EN MANTENIMIENTO</h1>
+                <h2>
+                    Esta parte de la pagina se encuentra bloqueada por el momento, por razones de seguridad y mantenimiento, para regresar, hacer click en la flecha de retroceso, en el navegador
+                </h2>
+                <h2 className="tagline">Precaución, zona de construcción:</h2>
+                <br />
+                <ImageWrapper>
+                    <img src={sample} alt={""} />
+                </ImageWrapper>
+                <a href="/">
+                <Button variant="contained" color="primary"> Regresar </Button>
+                </a>
+            </Container>
         </NfSection>
     );
 }
