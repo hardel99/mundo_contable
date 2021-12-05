07348de085e183a5193a94daaae54e8dbf7f57d0
M-Mag <villegasmkenya147@gmail.com>
2021-12-05 17:31:00 -0600

    Testimonial skeleton done

--- src/Components/ButtonElement.jsx
+++ src/Components/ButtonElement.jsx
@@ -1,36 +1,30 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Btn = styled.button`
-font-size: var(--type-button);
-font-weight: bold;
-padding: .5rem 1rem;
-display:block;
-border: none;
-border-radius:5px;
-background-color: var(--green);
-color: #fff;
-transition: 0.2s ease-out;
-outline: none;
-
-
-&:hover 
-   {
-    background-color: #fff;
-    color: var(--black);
-    border: 3px solid var(--green);
+    font-size: var(--type-button);
+    font-weight: bold;
+    padding: 0.5rem 1rem;
+    display: block;
+    border: none;
+    border-radius: 5px;
+    background-color: var(--green);
+    color: #fff;
     transition: 0.2s ease-out;
-    cursor: pointer;
-   }
+    outline: none;
 
-@media screen and (max-width: 760px){
-   font-size: 0.8rem;
-}
+    &:hover {
+        background-color: #fff;
+        color: var(--black);
+        border: 3px solid var(--green);
+        transition: 0.2s ease-out;
+        cursor: pointer;
+    }
 
+    @media screen and (max-width: 760px) {
+        font-size: 0.8rem;
+    }
 
-@media screen and (max-width: 425px){
-   font-size: 0.7rem;
-}
+    @media screen and (max-width: 425px) {
+        font-size: 0.7rem;
+    }
 `;
-
-
-
--- src/Components/Review-section/ReviewSection.jsx
+++ src/Components/Review-section/ReviewSection.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import Swiper, { Navigation, Pagination } from "swiper";
 import "./Review.css";
 
-export default function ReviewSection() {
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "vertical",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
+
+function ReviewSection() {
     return (
-        <div>
-            <h1>Hey, esta es la siguiente mision.... no la riegues</h1>
+        /* Slider main container */
+        <div class="swiper">
+            {/* Additional required wrapper*/}
+            <div class="swiper-wrapper">
+                {/*Slides*/}
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+            </div>
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
         </div>
     );
 }
+export default ReviewSection;
